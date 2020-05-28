2365144be77073cad6c475d126d54049488a2711
dathwo@outlook.com <dathwo@outlook.com>
2020-05-28 11:42:23 -0700

    Added validation to the order quantity to make sure it is a non negative integer

--- UserOrder/views.py
+++ UserOrder/views.py
@@ -3,6 +3,7 @@ from RestaurantEvent.models import *
 from UserOrder.models import *
 from UserLogin.models import *
 from RestaurantItem.models import *
+from django.contrib import messages
 import math
 
 debug = True
@@ -25,14 +26,20 @@ def storeOrder(request, eventID):
     if debug:
         request.session["userID"] = 1
     if "userID" in request.session:
-        user = User.objects.get(id=request.session["userID"])
-        event = Event.objects.get(id=eventID)
-        order = Order.objects.create(user = user, event=event, status="Unconfirmed")
-        for item_id, quantity in request.POST.items():
-            if item_id != "csrfmiddlewaretoken":
-                if int(quantity) > 0:
-                    OrderQuantity.objects.create(item=Item.objects.get(id=int(item_id)), order = order, quantity= quantity)
-        return redirect(f"/user/order/{event.id}/{order.id}/review")
+        errors = OrderQuantity.objects.validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value, extra_tags = key)
+            return redirect(f"/user/order/{eventID}/new")
+        else:
+            user = User.objects.get(id=request.session["userID"])
+            event = Event.objects.get(id=eventID)
+            order = Order.objects.create(user = user, event=event, status="Unconfirmed")
+            for item_id, quantity in request.POST.items():
+                if item_id != "csrfmiddlewaretoken":
+                    if int(quantity) > 0:
+                        OrderQuantity.objects.create(item=Item.objects.get(id=int(item_id)), order = order, quantity= quantity)
+            return redirect(f"/user/order/{event.id}/{order.id}/review")
     else:
         return redirect("/")
 
